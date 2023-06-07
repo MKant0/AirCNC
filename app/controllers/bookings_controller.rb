8f33e06f7805f813dd59926a0566a1eb16956ee9
Mkant0 <maximilian.kaiser.a@gmail.com>
2023-06-06 21:26:20 -0400

    controlador de booking creado con los policy de booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,21 +1,25 @@
 class BookingsController < ApplicationController
   before_action :set_booking, only: [:show, :edit, :update, :destroy]
-  before_action :set_user, only: [:new, :create]
-
+  before_action :set_chair, only: [:new, :create]
+# al ser la ruta nesteada con chair, usamos el chair_id para encontrar la silla
   def index
-    @bookings = Booking.where(user_id: @user.id)
+    @bookings = policy_scope(Booking)
   end
 
   def show
+    autorize @booking
   end
 
   def new
     @booking = Booking.new
+    autorize @booking
   end
 
   def create
     @booking = Booking.new(booking_params)
-    @booking.user = @user
+    @booking.chair = @chair
+    @booking.user = current_user
+    autorize @booking
     if @booking.save
       redirect_to booking_path(@booking)
     else
@@ -24,9 +28,11 @@ class BookingsController < ApplicationController
   end
 
   def edit
+    autorize @booking
   end
 
   def update
+    autorize @booking
     if @booking.update(booking_params)
       redirect_to booking_path(@booking)
     else
@@ -35,6 +41,7 @@ class BookingsController < ApplicationController
   end
 
   def destroy
+    autorize @booking
     @booking.destroy
     redirect_to bookings_path, status: :see_other
   end
@@ -42,14 +49,14 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:booking).permit(:users_id, :chairs_id, :date_start, :date_end)
+    params.require(:booking).permit(:date_start, :date_end)
   end
 
   def set_booking
     @booking = Booking.find(params[:id])
   end
-
-  def set_user
-    @user = User.find(params[:user_id])
+# al ser la ruta nesteada con chair, usamos el chair_id para encontrar la silla
+  def set_chair
+    @chair = Chair.find(params[:chair_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'bookings/index'
-  get 'bookings/show'
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/edit'
-  get 'bookings/update'
-  get 'bookings/destroy'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
