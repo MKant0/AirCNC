21561274b619d9fb73bb208459299a8e9cc0eaf2
esteban araya <eaaraya@gmail.com>
2023-06-07 18:56:11 -0400

    termine el controlador, esperanod la instalacion de pundit

--- app/controllers/chairs_controller.rb
+++ app/controllers/chairs_controller.rb
@@ -1,2 +1,52 @@
 class ChairsController < ApplicationController
+  before_action :set_chair, only: %i[show edit update destroy]
+  def index
+
+  end
+
+  def show
+    authorize @chair
+  end
+
+  def new
+    @chair = Chair.new
+    authorize @chair
+  end
+
+  def create
+    @chair = Chair.new(chair_params)
+    @chair.user = current_user
+    authorize @chair
+    if @chair.save
+      redirect_to chair_path(@chair)
+    else
+      render :new, status: :unporcessable_entity
+    end
+  end
+
+
+  def edit
+    authorize @chair
+  end
+
+  def update
+    authorize @chair
+    @chair.update(chair_params)
+  end
+
+  def destroy
+    authorize @chair
+    @chair.destroy
+    redirect_to chair_path, status: :see_other
+  end
+
+  private
+
+  def set_chair
+    @chair = Chair.find(params[:id])
+  end
+
+  def chair_params
+    params.require(:chair).permit(:name, :address, :description, :price)
+  end
 end
