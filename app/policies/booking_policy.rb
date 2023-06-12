3d49c8ba52af3047b14582e0b1b93ecfea379aaf
esteban araya <eaaraya@gmail.com>
2023-06-12 01:59:20 -0400

    en las views el usuario que postea una silla ya no puede reservar la suya y ahora tambien puede ver el boton de borrar y editar

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -3,17 +3,20 @@ class BookingPolicy < ApplicationPolicy
     # NOTE: Be explicit about which records you allow access to!
     def resolve
       scope.all
+      # scope.where(user != user)
     end
   end
+
   def show?
     true
   end
 
   def new?
-    true
+    create?
   end
 
   def create?
+    #record.chair.user_id != user
     true
   end
 
