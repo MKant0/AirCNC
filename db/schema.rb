579703a9a16dd46631935defef3d9f1cbafaf755
esteban araya <eaaraya@gmail.com>
2023-06-13 10:27:45 -0400

    agregue la columna url_image a la db de user. Por ahora no hice el name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_10_161024) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_13_141708) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -74,6 +74,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_10_161024) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url_image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
