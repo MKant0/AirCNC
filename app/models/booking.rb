86f663a7f87d7d5680191d10fb7e82ff17726163
José Tom´ás Otero García <otero.josetomas@gmail.com>
2023-06-17 16:01:22 -0400

    ultimos arreglos del sign up

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,11 @@ class Booking < ApplicationRecord
   belongs_to :chair
 
   validates :date_start, :date_end, presence: true
+  validate :date_end_after_date_start
+
+  def date_end_after_date_start
+    if date_end < date_start
+      errors.add(:date_end, "can't be before date start")
+    end
+  end
 end
