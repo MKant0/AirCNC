20be9c6670ce668c94ef2fe7ddddde5e44fa0a87
esteban araya <eaaraya@gmail.com>
2023-06-17 17:21:39 -0400

    added mt to views

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -6,12 +6,11 @@ export default class extends Controller {
     console.log("hello from the nav bar controller")
   }
 
-  updateNavbar() {
-    if (window.scrollY >= 0) {
-      this.element.classList.add("navbar-lewagon-white")
-    } else {
-      this.element.classList.remove("navbar-lewagon-white")
-    }
-
-  }
+  // updateNavbar() {
+  //   if (window.scrollY >= 0) {
+  //     this.element.classList.add("navbar-lewagon-white")
+  //   } else {
+  //     this.element.classList.remove("navbar-lewagon-white")
+  //   }
+  // }
 }
