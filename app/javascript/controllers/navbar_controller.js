bf686c6df3c6b2a706e3da639d6f06ad1fc26ee3
esteban araya <eaaraya@gmail.com>
2023-06-17 16:57:02 -0400

    tenia un problema con el branch

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="navbar"
+export default class extends Controller {
+  connect() {
+    console.log("hello from the nav bar controller")
+  }
+
+  updateNavbar() {
+    if (window.scrollY >= 0) {
+      this.element.classList.add("navbar-lewagon-white")
+    } else {
+      this.element.classList.remove("navbar-lewagon-white")
+    }
+
+  }
+}
